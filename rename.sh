609de8225335fb0d22cac439b111cbb96d0e07d2
Nikolay <77804859+Shaposhnikov-Nick@users.noreply.github.com>
2024-08-13 19:29:28 +0300

    improve script

--- rename.sh
+++ rename.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
-
 # rename file
 
-cp $1 $2
-rm $1
+# check arguments
+if [ $# -ne 2 ]; then
+	echo "Error! Need 2 arguments"
+	exit 1
+fi
+
+# check file exists
+if [ ! -f "$1" ]; then
+	echo "$1 is not a file or does not exists"
+	exit 1
+fi
+
+# check new file name
+if [ ! -n "$2" ]; then
+	echo "Empty new file name"
+	exit 1
+fi
+
+# copy file with new name
+if cp "$1"  "$2" && rm -f "$1" ; then
+	echo "File renamed"
+	exit 0
+else
+	echo "Error!"
+	exit 1
+fi
