c9a7c3a90e385edf5d0c83ab7cd91eddd0fd6afb
Nikolay <77804859+Shaposhnikov-Nick@users.noreply.github.com>
2024-08-21 11:46:42 +0300

    add new operators

--- calculator.sh
+++ calculator.sh
@@ -7,5 +7,14 @@ read -p "Enter operator: " operator
 case $operator in
 	"+" ) echo " $val1 + $val2 =" $(expr "$val1" + "$val2") ;;
 	"*" ) echo " $val1 * $val2 =" $(expr "$val1" \* "$val2") ;;
+	"-" ) echo " $val1 - $val2 =" $(expr "$val1" - "$val2") ;;
+	"/" ) if [ "$val2" -eq 0 ]; then 
+			echo "Divide by zero!"
+			exit 1
+		else
+			echo " $val1 / $val2 =" $(expr "$val1" / "$val2")
+		fi ;;
+	"**" ) echo " $val1 ** $val2 =" $(( "$val1" ** "$val2" )) ;;
+
 	* )  echo "unknown operator!"
 esac  
