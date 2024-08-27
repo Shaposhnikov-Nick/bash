785f1964a620e0d3fbeb44426c8a8c293129e1e4
Nikolay <77804859+Shaposhnikov-Nick@users.noreply.github.com>
2024-08-27 13:21:30 +0300

    add sqrt

--- calculator.sh
+++ calculator.sh
@@ -4,6 +4,10 @@ read -p "Enter first value: " val1
 read -p "Enter second value: " val2
 read -p "Enter operator: " operator 
 
+sqrt() {
+	echo sqrt "($1)" | bc -l
+}
+
 case $operator in
 	"+" ) echo " $val1 + $val2 =" $(expr "$val1" + "$val2") ;;
 	"*" ) echo " $val1 * $val2 =" $(expr "$val1" \* "$val2") ;;
@@ -15,6 +19,6 @@ case $operator in
 			echo " $val1 / $val2 =" $(expr "$val1" / "$val2")
 		fi ;;
 	"**" ) echo " $val1 ** $val2 =" $(( "$val1" ** "$val2" )) ;;
-
+	"sqrt" ) echo "sqrt($val1) =" $(sqrt $val1) ;;
 	* )  echo "unknown operator!"
 esac  
